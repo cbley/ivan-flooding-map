3afffe77cf371fbf283df9fdb6207a0b77f35a81
Christopher Bley <chrisbley@gmail.com>
2015-11-08 09:53:32 -0500

    Properly set GeoJSON content-type header when uploading to S3

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var awspublish = require('gulp-awspublish');
+var awspublishRouter = require('gulp-awspublish-router');
 
 gulp.task('upload', function upload() {
   var publisher = awspublish.create({
@@ -16,6 +17,18 @@ gulp.task('upload', function upload() {
   return gulp.src('dist/**')
     .pipe(rename(function (path) {
       path.dirname = 'projects/ivan/' + path.dirname;
+    }))
+    .pipe(awspublishRouter({
+      routes: {
+        // set correct Content-Type for GeoJSON files
+        '\.geojson$': {
+          "headers": {
+            "Content-Type": "application/json"
+          }
+        },
+        // pass-through for everything else
+        '^.+$': '$&'
+      }
     }))
     .pipe(awspublish.gzip({ext: ''}))
     .pipe(publisher.publish(headers))
